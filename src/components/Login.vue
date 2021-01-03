1797779ad82635d09b45b64a43972f41c98a0b48
Eron9528 <1551447510@qq.com>
2021-01-03 20:37:04 +0800

    登录页面

---
+++ src/components/Login.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+        <div class="avatar_box">
+            <img src="../assets/logo.png" alt="">
+        </div>
+        <el-form ref="loginFormRef" :model="loginForm" :rules="rules" label-width="0px" class="login_form">
+            <!-- 用户名 -->
+            <el-form-item prop="username">
+                <el-input v-model="loginForm.username" placeholder="请输入用户名" prefix-icon="el-icon-user"></el-input>
+            </el-form-item>
+            <!-- 密码 -->
+            <el-form-item prop="password">
+                <el-input v-model="loginForm.password" type='password' placeholder="请输入密码" prefix-icon="el-icon-s-goods"></el-input>
+            </el-form-item>
+            <el-form-item class="btns">
+                <el-button type="primary" @click="login">登录</el-button>
+                <el-button type="info" @click="loginFormRef" >重置</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名称', trigger: 'blur' },
+          { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    loginFormRef () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login_box {
+    background-color: #fff;
+    width: 450px;
+    height: 300px;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    .avatar_box{
+        width: 130px;
+        height: 130px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #ddd;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        background-color: #fff;
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+}
+
+.btns {
+    display: flex;
+    justify-content: flex-end;
+}
+
+.login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+</style>
